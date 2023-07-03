704a204635c72a1b341fdb488f05014df8943084
Alexander Resch <alexander@resch-web.at>
2023-07-03 11:29:38 +0200

    Initial commit

---
+++ UsbDataReceiver/Extensions.cs
@@ -0,0 +1,9 @@
+﻿namespace UsbDataReceiver.Extensions;
+
+public static class Extensions
+{
+    public static double Round(this double value, int digits)
+    {
+        return Math.Round(value, digits, MidpointRounding.AwayFromZero);
+    }
+}
---
+++ UsbDataReceiver/Program.cs
@@ -0,0 +1,43 @@
+﻿using NationalInstruments.DAQmx;
+using Task = NationalInstruments.DAQmx.Task;
+using UsbDataReceiver.Extensions;
+
+Task voltageInTask = new();
+Task ampereInTask = new();
+Task temperatureInTask = new();
+
+//setup channels
+var voltageAiChannel = voltageInTask.AIChannels.CreateVoltageChannel(
+    "Dev2/ai0",
+    "VoltageChannel",
+    AITerminalConfiguration.Rse,
+    0,
+    10,
+    AIVoltageUnits.Volts);
+var ampereAiChannel = ampereInTask.AIChannels.CreateVoltageChannel(
+    "Dev2/ai1",
+    "AmpereChannel",
+    AITerminalConfiguration.Rse,
+    0,
+    10,
+    AIVoltageUnits.Volts);
+var temperatureAiChannel = temperatureInTask.AIChannels.CreateVoltageChannel(
+    "Dev2/ai2",
+    "TemperatureChannel",
+    AITerminalConfiguration.Rse,
+    0,
+    10,
+    AIVoltageUnits.Volts);
+
+AnalogSingleChannelReader reader = new(voltageInTask.Stream);
+AnalogSingleChannelReader reader2 = new(ampereInTask.Stream);
+AnalogSingleChannelReader reader3 = new(temperatureInTask.Stream);
+while (true)
+{
+    var volts = reader.ReadSingleSample().Round(2);
+    var ampere = reader2.ReadSingleSample().Round(2);
+    var temperature = reader3.ReadSingleSample().Round(2);
+    Console.WriteLine($"volts: {volts}, ampere: {ampere}, temperature: {temperature}");
+    
+}
+
